c86b5c2a98c95948bf294d3f8fb281d4c00e3cb3
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-05-21 14:57:31 +0530

    Added a Looging class
    Added a Looging class to help the application have the logs

---
+++ ScreenActivator/Buisness/Logging.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Logger;
+
+namespace ScreenActivator.Buisness
+{
+    public class Logging
+    {
+        public ILogger Log
+        {
+            get { return Logger.Log.Logger; }
+        }
+    }
+}
