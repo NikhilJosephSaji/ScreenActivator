8e849691065084beb6d7d290a67901d1ce6b1f12
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-05-21 15:00:50 +0530

    Create ScreenActivatorGlobal.cs
    so that we use these value in the Entire application so see the settings

---
+++ ScreenActivator/Buisness/ScreenActivatorGlobal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ScreenActivator.Buisness
+{
+    public struct ScreenActivatorGlobal
+    {
+        public bool DisableMicroPhone;
+        public bool DisableSpeaker;
+        public bool EnableSound;
+        public bool EnableMinimize;
+        public bool EnableScreenDrag;
+        public bool EnableSpeech;
+        public bool EnableLog;
+    }
+}
