b4b9ee269e654022ba726172e560b9598e8534d1
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-05-21 15:06:27 +0530

    Added New Xaml
    Added new Xaml
    1.AdminScreen.xaml
    2.AuthenticateView.xaml
    Both xaml have many function implemented that is
    1. sound
    2.speech
    3.control for all settings

---
+++ ScreenActivator/View/AdminScreen.xaml.cs
@@ -0,0 +1,89 @@
+﻿using ScreenActivator.Buisness;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
+using System.Xml.Linq;
+using Msg = CustomMessageBox;
+
+namespace ScreenActivator
+{
+    /// <summary>
+    /// Interaction logic for AdminScreen.xaml
+    /// </summary>
+    public partial class AdminScreen : Window
+    {
+        private MainWindow _win;
+        private XmlHelper xml;
+        public AdminScreen(MainWindow win)
+        {
+            _win = win;
+            InitializeComponent();
+            _win.speech?.Speak("Admin Window Opened");
+        }
+
+        private void closebtn_Click(object sender, RoutedEventArgs e)
+        {
+            _win.speech?.Speak("Admin window Closed");
+            _win.sound?.ClickSound();
+            _win.AdminScreenCount = 0;
+            this.Close();
+            _win.Show();
+        }
+
+        private void FeedBack_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            _win.sound?.ClickSound();
+            _win.speech?.Speak("Save Button Clicked");
+            xml.Xml.Element("DisableMicroPhone").Value = DisableMicroPhone.IsChecked.ToString();
+            xml.Xml.Element("DisableSpeaker").Value = DisableSpeaker.IsChecked.ToString();
+            xml.Xml.Element("EnableSound").Value = EnableSound.IsChecked.ToString();
+            xml.Xml.Element("EnableMinimize").Value = EnableMinimize.IsChecked.ToString();
+            xml.Xml.Element("EnableScreenDrag").Value = EnableScreenDrag.IsChecked.ToString();
+            xml.Xml.Element("EnableSpeech").Value = EnableSpeech.IsChecked.ToString();
+            xml.Xml.Element("EnableLog").Value = EnableLog.IsChecked.ToString();
+            if (xml.SaveXml() == 1)
+                Msg.CustomMessageBox.Show("Settings Saved Sucessfully !");
+            _win.GetXml();
+            _win.ApplySettings();
+        }
+
+        private void AdminScreen_Loaded(object sender, RoutedEventArgs e)
+        {
+            xml = new XmlHelper();
+            DisableMicroPhone.IsChecked = xml.ConvertXmlStringToBool(xml.Xml.Element("DisableMicroPhone").Value);
+            DisableSpeaker.IsChecked = xml.ConvertXmlStringToBool(xml.Xml.Element("DisableSpeaker").Value);
+            EnableSound.IsChecked = xml.ConvertXmlStringToBool(xml.Xml.Element("EnableSound").Value);
+            EnableMinimize.IsChecked = xml.ConvertXmlStringToBool(xml.Xml.Element("EnableMinimize").Value);
+            EnableScreenDrag.IsChecked = xml.ConvertXmlStringToBool(xml.Xml.Element("EnableScreenDrag").Value);
+            EnableSpeech.IsChecked = xml.ConvertXmlStringToBool(xml.Xml.Element("EnableSpeech").Value);
+            EnableLog.IsChecked = xml.ConvertXmlStringToBool(xml.Xml.Element("EnableLog").Value);
+        }
+
+        private void EnableSoundAndSpeech_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleButton tb = (ToggleButton)sender;
+            if (tb.IsChecked.Value)
+            {
+                if (tb.Name == EnableSound.Name)
+                {
+                    if (EnableSpeech.IsChecked.Value)
+                        EnableSpeech.IsChecked = false;
+                }
+                else
+                {
+                    if (EnableSound.IsChecked.Value)
+                        EnableSound.IsChecked = false;
+                }
+            }
+        }
+    }
+}
---
+++ ScreenActivator/View/AuthenticateView.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ScreenActivator
+{
+    /// <summary>
+    /// Interaction logic for AuthenticateView.xaml
+    /// </summary>
+    public partial class AuthenticateView : Window
+    {
+        private MainWindow _win;
+        public AuthenticateView(MainWindow win)
+        {
+            _win = win;
+            InitializeComponent();
+            ErrorMsg.Visibility = Visibility.Collapsed;
+            _win.speech?.Speak("Authenticate Window Opened");
+        }
+
+        private bool valid()
+        {
+            if (ValidateUsserName.Text == null || ValidateUsserName.Text == string.Empty)
+            {
+                ErrorMsg.Visibility = Visibility.Visible;
+                ErrorMsg.Content = "Please Enter UsserName";
+                _win.speech?.Speak(ErrorMsg.Content.ToString());
+                return false;
+            }
+
+            if (validatePassword.Password == null || validatePassword.Password == string.Empty)
+            {
+                ErrorMsg.Visibility = Visibility.Visible;
+                ErrorMsg.Content = "Please Enter Password";
+                _win.speech?.Speak(ErrorMsg.Content.ToString());
+                return false;
+            }
+
+            return true;
+        }
+
+        private void Validate_Click(object sender, RoutedEventArgs e)
+        {
+            _win.sound?.ClickSound();            
+            if (!valid())
+                return;
+            if (ValidateUsserName.Text == "admin" && validatePassword.Password == "admin")
+            {
+                _win.speech?.Speak("Validate User");
+                Thread.Sleep(1000);
+                this.Close();
+                new AdminScreen(_win).Show();
+            }
+            else
+            {
+                ErrorMsg.Visibility = Visibility.Visible;
+                ErrorMsg.Content = "Authentication Failed";
+                _win.speech?.Speak(ErrorMsg.Content.ToString());
+            }
+        }
+
+        private void ValidateCloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            _win.sound?.ClickSound();
+            _win.speech?.Speak("Authenticate Window Closed");
+            _win.AdminScreenCount = 0;
+            this.Close();
+        }
+
+        private void validate_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Validate_Click(sender, null);
+            }
+        }
+    }
+}
