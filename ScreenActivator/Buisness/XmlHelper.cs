fbb2ab878a63fc959357d2d4ed16ae225469fb73
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-05-21 14:55:34 +0530

    Added an Xml Helper
    Added an Xml Helper to Fetch and Update the Data

---
+++ ScreenActivator/Buisness/XmlHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using System.Xml.Linq;
+
+namespace ScreenActivator.Buisness
+{
+    public class XmlHelper
+    {
+        private XElement xml;
+        private string path;
+
+        public XElement Xml { get { return xml != null ? xml : null; } }
+        public XmlHelper()
+        {
+            path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            xml = XElement.Load(path + "\\Buisness\\ScreenActData.xml");
+        }
+
+        public bool ConvertXmlStringToBool(string str)
+        {
+            return Convert.ToBoolean(str);
+        }
+
+        public int SaveXml()
+        {
+            try
+            {
+                var path = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+                path = path + "\\Buisness\\ScreenActData.xml";
+                xml.Save(path);
+                return 1;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+    }
+}
