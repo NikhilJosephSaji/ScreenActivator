cd9c7da6b60663a872653450232e05bf22b5507d
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-05-21 14:56:12 +0530

    Added a Speech Class
    Added a Speech Class to help the application to Speak the contents

---
+++ ScreenActivator/Buisness/Speech.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenActivator.Buisness
+{
+    public class Speech
+    {
+        public void SpeakAsync(string text, bool wait = false)
+        {
+            ExecuteCommand(
+                $@"Add-Type -AssemblyName System.speech; 
+                $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer;
+                $speak.Rate = 1;
+                $speak.Speak(""{text}"");");
+
+            void ExecuteCommand(string command)
+            {
+                string path = Path.GetTempPath() + Guid.NewGuid() + ".ps1";
+
+                // make sure to be using System.Text
+                using (StreamWriter sw = new StreamWriter(path, false, Encoding.UTF8))
+                {
+                    sw.Write(command);
+
+                    ProcessStartInfo start = new ProcessStartInfo()
+                    {
+                        FileName = @"C:\Windows\System32\windowspowershell\v1.0\powershell.exe",
+                        LoadUserProfile = false,
+                        UseShellExecute = false,
+                        CreateNoWindow = true,
+                        Arguments = $"-executionpolicy bypass -File {path}",
+                        WindowStyle = ProcessWindowStyle.Hidden
+                    };
+
+                    Process process = Process.Start(start);
+
+                    if (wait)
+                        process.WaitForExit();
+                }
+            }
+        }
+
+        public void Speak(string str)
+        {
+            SpeakAsync(str);
+        }
+    }
+}
