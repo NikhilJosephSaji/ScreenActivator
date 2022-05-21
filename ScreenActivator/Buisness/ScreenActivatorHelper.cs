acc9b87acaf3a2839b319069a387a70f276b0d6b
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2022-05-21 14:59:27 +0530

    Create ScreenActivatorHelper.cs
    Where we wrote the global function to help the application

---
+++ ScreenActivator/Buisness/ScreenActivatorHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+
+namespace ScreenActivator.Buisness
+{
+    public class ScreenActivatorHelper
+    {
+        private MainWindow _win;
+        public ScreenActivatorHelper(MainWindow win)
+        {
+            _win = win;
+        }
+        public void DisableEnableMicButton(bool isNeeded)
+        {
+            if (isNeeded)
+            {
+                _win.MuteMicrophone.Background = Brushes.SlateBlue;
+                _win.MuteMicrophone.Click -= new RoutedEventHandler(_win.MuteMicrophone_Click);
+            }
+            else
+            {
+                _win.MuteMicrophone.Background = Brushes.LightBlue;
+                _win.MuteMicrophone.Click += new RoutedEventHandler(_win.MuteMicrophone_Click);
+            }
+        }
+
+        public void DisableEnableSpeaker(bool isNeeded)
+        {
+            if (isNeeded)
+            {
+                _win.SpeakerBtn.Background = Brushes.SlateBlue;
+                _win.SpeakerBtn.Click -= new RoutedEventHandler(_win.SpeakerBtn_Click);
+            }
+            else
+            {
+                _win.SpeakerBtn.Background = Brushes.LightBlue;
+                _win.SpeakerBtn.Click += new RoutedEventHandler(_win.SpeakerBtn_Click);
+            }
+        }
+
+        const int WM_SYSCOMMAND = 0x0112;
+        const int SC_MOVE = 0xF010;       
+
+        public IntPtr WndProcNoMove(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+
+            switch (msg)
+            {
+                case WM_SYSCOMMAND:
+                    int command = wParam.ToInt32() & 0xfff0;
+                    if (command == SC_MOVE)
+                    {
+                        handled = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return IntPtr.Zero;
+        }
+    }
+}
